30b4a03d390ad0747c3c49a20074da761193d431
Guest User <Guest@Epicodus-6D.local>
2018-02-22 15:49:12 -0800

    finalized

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -36,6 +36,7 @@ namespace ToDoList
           name: "default",
           template: "{controller=Home}/{action=Index}/{id?}");
       });
+      app.UseStaticFiles();
       app.Run(async (context) =>
       {
         await context.Response.WriteAsync("Hello World!");
